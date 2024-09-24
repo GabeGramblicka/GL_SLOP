c55847ef17e1dac31f03fa42a2c5adbf3afb7fc0
Gabriel Gramblicka <gabegramblicka@protonmail.com>
2024-09-24 12:41:19 -0700

    created window and context

--- GLSLOP-Proj/Main.cpp
+++ GLSLOP-Proj/Main.cpp
@@ -11,6 +11,8 @@
 #include <SDL2\SDL.h>
 #include <SDL2\SDL_main.h>
 
+#include <math.h>
+
 //------------------------------------------------------------------------------
 // Private Constants:
 //------------------------------------------------------------------------------
@@ -44,7 +46,31 @@
 //------------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) {
-	//SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("GLSLOP", SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, 600, 600,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	bool done = false;
+	Uint32 ticksLast = SDL_GetTicks();
+	while (!done) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				done = true;
+				break;
+			}
+		}
+		Uint32 ticks = SDL_GetTicks();
+		double dt = 0.001 * (ticks - ticksLast);
+		ticksLast = ticks;
+		SDL_GL_SwapWindow(window);
+	}
+	SDL_GL_DeleteContext(context);
+	SDL_Quit();
 	return 0;
 }
+
 
