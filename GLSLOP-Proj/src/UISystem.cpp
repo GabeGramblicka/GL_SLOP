32fda85c7db26f5e96d7f090457a75d0fdb07359
Gabriel Gramblicka <gabegramblicka@protonmail.com>
2024-10-01 22:26:47 -0700

    framebuffer works but might not even use it

--- GLSLOP-Proj/src/UISystem.cpp
+++ GLSLOP-Proj/src/UISystem.cpp
@@ -89,20 +89,83 @@ void UISystem::Update(float dt) {
 	ImGui_ImplSDL2_NewFrame();
 	ImGui::NewFrame();
 
+	static ImVec2 s_gamePos;
+	static ImVec2 s_gameSize = { 180, 200 };
+
 	if (s_showDemoWindow) {
-		ImGui::ShowDemoWindow(&s_showDemoWindow);
+		//ImGui::ShowDemoWindow(&s_showDemoWindow);
+	//}
+	//
+	//{
+		// Pre-Window Variables and Settings
+		glm::ivec2 appSize = WindowSystem::WindowSize();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0, 0 });
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0);
+
+		// So it's easier to drag
+		ImGui::SetNextWindowSize(ImVec2(appSize.x, appSize.y));
+
+		// Create window
+		if (ImGui::Begin("Game Preview"))
+		{
+			ImVec2 contentRegion = ImGui::GetContentRegionAvail();
+			s_gamePos = ImGui::GetWindowPos();
+
+			// Calculate the aspect ratio of the image and the actual window
+			float imageAspectRatio = static_cast<float>(appSize.x) / static_cast<float>(appSize.y);
+			float paddingAspectRatio = contentRegion.x / contentRegion.y;
+
+			// Set the viewport 
+			glViewport(0, 0, appSize.x, appSize.y);
+
+			// Either the x or the y will be adjusted but not both
+			ImVec2 padding{ 0.0f, 0.0f };
+
+			// Scaling window horizontally
+			if (paddingAspectRatio >= imageAspectRatio)
+			{
+				float imageWidth = contentRegion.y * imageAspectRatio;
+				padding.x = (contentRegion.x - imageWidth) / 2;
+				s_gameSize = { imageWidth , contentRegion.y };
+
+				ImVec2 cursorPos = ImGui::GetCursorPos() + ImVec2{ padding.x, 0 };
+				s_gamePos += cursorPos;
+				ImGui::SetCursorPosX(cursorPos.x);
+			}
+			// Scaling window vertically
+			else // (paddingAspectRatio < imageAspectRatio)
+			{
+				float imageHeight = contentRegion.x / imageAspectRatio;
+				padding.y = (contentRegion.y - imageHeight) / 2;
+				s_gameSize = { contentRegion.x, imageHeight };
+
+				float cursorPosY = ImGui::GetCursorPosY() + padding.y;
+				s_gamePos.y += cursorPosY;
+				ImGui::SetCursorPosY(cursorPosY);
+			}
+			// Finally render the frame buffer
+			ImGui::Image((ImTextureID)(intptr_t)PGE::FrameBuffer::GetTextureId(), s_gameSize, { 0, 1 }, { 1, 0 });
+
+			//ImGui::GetForegroundDrawList()->AddRect(_gamePos, _gamePos + _gameSize, WarningColorU32());
+		}
+		ImGui::End();
+
+		// Window padding and rounding
+		ImGui::PopStyleVar(2);
 	}
+
 	ImGui::EndFrame();
 }
 
 void UISystem::Render() const {
-	// PGE::FrameBuffer::UnbindFrameBuffer();
+	PGE::FrameBuffer::UnbindFrameBuffer();
 
 	PGE::ClearBackground();
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-	// PGE::FrameBuffer::BindFrameBuffer();
+	PGE::FrameBuffer::BindFrameBuffer();
 
 	ImGuiIO& io = ImGui::GetIO();
 	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
