8a8d612b363156caa8925e006b70fddf4d0406f8
Gabriel Gramblicka <gabegramblicka@protonmail.com>
2024-10-28 14:52:05 -0700

    looks good. push to prod

--- GLSLOP-Proj/src/Parser.cpp
+++ GLSLOP-Proj/src/Parser.cpp
@@ -42,6 +42,20 @@
 // Private Functions:
 //------------------------------------------------------------------------------
 
+const char* GetPath(const char* shaderText, size_t begin, size_t end, size_t size) {
+  char* s1 = (char*)calloc(1, begin);
+  char* s2 = (char*)calloc(1, size - end);
+  for (size_t i = 0; i < begin; ++i) {
+    s1[i] = shaderText[i];
+  }
+
+  for (size_t i = end; i < size; ++i) {
+    s2[i] = shaderText[i];
+  }
+
+  return nullptr;
+}
+
 const char* glSlopParse(const char* shaderText) {
   const char* includeString = "#include";
   size_t iSize = strlen(includeString) /* 8 */; // include length
@@ -53,9 +67,12 @@ const char* glSlopParse(const char* shaderText) {
   for (int i = 0; i < oSize; ++i) {
     Data::OB() << shaderText[i];
 
+    size_t beginFind = i;
+
     for (int j = 0; j < iSize; ++j) {
       if (j == iSize - 1) {
         std::cout << "Found include" << std::endl;
+        const char* path = GetPath(shaderText, beginFind, i, oSize);
       }
       if (shaderText[i] != includeString[j]) {
         j = 0; // reset
@@ -70,4 +87,5 @@ const char* glSlopParse(const char* shaderText) {
 
   return newFile;
 }
+
 
